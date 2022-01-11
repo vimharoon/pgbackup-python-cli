f239924cce1e5d3c48e3e3a68f47c8856f4d9975
Haroon Mohammad <kakashi24@live.fr>
2022-01-11 21:23:48 +0000

    change repo name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/vimharoon/pgbackup',
+    url='https://github.com/vimharoon/pgbackup-python-cli',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['boto3'],
